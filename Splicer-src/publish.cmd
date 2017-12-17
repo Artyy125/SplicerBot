bf439b8998e34024207a92987aebe9963451ae20
Alireza Torkamani <ATorkamani@dacgroup.com>
2017-12-17 09:57:43 -0500

    first commit

---
+++ Splicer-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=splicer-Web-Deploy.pubxml -p:Password=3DvzoSQcv149DLRJf56u2333JQW6hagWSxKrWBatq9iRdlTgHBoSYjxkZcuK
+
