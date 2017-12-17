f5a605d27f4f111fb30b53fdecdadb08c7a4cd31
Alireza Torkamani <ATorkamani@dacgroup.com>
2017-12-17 11:43:05 -0500

    adding new conditions

--- SplicerBot/Dialogs/EchoDialog.cs
+++ SplicerBot/Dialogs/EchoDialog.cs
@@ -20,22 +20,41 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
 
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
-            var message = await argument;
-
-            if (message.Text == "reset")
+            try
             {
-                PromptDialog.Confirm(
-                    context,
-                    AfterResetAsync,
-                    "Are you sure you want to reset the count?",
-                    "Didn't get that!",
-                    promptStyle: PromptStyle.Auto);
+                var message = await argument;
+                string userId = message.Id;
+                string chatId = message.ChannelId;
+                string[] messageData = message.Text.Split('@');
+                //HttpClient client = new HttpClient();
+                //client.BaseAddress = new Uri(URL);
+                if (messageData[0] == "/reset")
+                {
+                    PromptDialog.Confirm(
+                        context,
+                        AfterResetAsync,
+                        "Are you sure you want to reset the count?",
+                        "Didn't get that!",
+                        promptStyle: PromptStyle.Auto);
+                }
+                else if (messageData[0] == "/report")
+                {
+                    await context.PostAsync("The reports will be available soon." );
+                    context.Wait(MessageReceivedAsync);
+                }
+                else
+                {
+                    await context.PostAsync($"{this.count++}: This bot is not completed yet!");
+                    context.Wait(MessageReceivedAsync);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                await context.PostAsync("error Message:" + ex.Message + "%%%" + ex.InnerException + "%%%" + ex.StackTrace);
                 context.Wait(MessageReceivedAsync);
+                throw;
             }
+            
         }
 
         public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
